f1d5080848f424d52bf4bf7bd7e34d1d417b5739
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-01-31 17:19:17 +0545

    added pages and navigated between them using react-route. Made Header component seperate and used it in our pages. Used <Link> insted of <a>.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,18 @@
 import { HomePage } from './pages/HomePage';
+import { OrdersPage } from './pages/OrdersPage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { TrackingPage } from './pages/TrackingPage';
+import { Routes, Route} from 'react-router';
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
   )
 }
 

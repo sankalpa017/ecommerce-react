84c35ac694f6a3f8014209c7e8c6c59a67e6cd15
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-01 16:34:27 +0545

    used favicon in tracking page and replaced anchor elem with link elem

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,16 +1,18 @@
 import { Header } from '../components/Header';
+import { Link } from 'react-router';
 import './TrackingPage.css';
  
 export function TrackingPage() {
   return (
     <>
       <title>Product Tracking</title>
+      <link rel="icon" href="images/tracking-favicon.png" />
       <Header />
       <div class="tracking-page">
         <div class="order-tracking">
-          <a class="back-to-orders-link link-primary" href="orders">
+          <Link class="back-to-orders-link link-primary" to="/orders">
             View all orders
-          </a>
+          </Link>
 
           <div class="delivery-date">
             Arriving on Monday, June 13

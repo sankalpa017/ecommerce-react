811fad0667b353b8e8675c4d476bba4042ede918
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-02 15:06:36 +0545

    used data from backend to generate HTML

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,20 @@
 import './HomePage.css';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
+// import { products } from '../../starting-code/data/products';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 
 export function HomePage() {
-  axios('http://localhost:3000/api/products')
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios('http://localhost:3000/api/products')
     .then((response) => {
-      console.log(response.data);
+      setProducts(response.data);
     })
+  }, []);
+  
   return (
   <>
     <title>Ecommerce Project</title>

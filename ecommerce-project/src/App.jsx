9996a875beef91815c144f0c39191f07d0d3bf3d
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-05 17:09:06 +0545

    Passed cart as a param to Not Found page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <Route path="orders" element={<OrdersPage cart={cart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
-      <Route path="*" element={<PageNotFound />} />
+      <Route path="*" element={<PageNotFound cart={cart}/>} />
     </Routes>
   )
 }
--- ecommerce-project/src/pages/PageNotFound.jsx
+++ ecommerce-project/src/pages/PageNotFound.jsx
@@ -1,12 +1,12 @@
 import { Header } from '../components/Header';
 import './PageNotFound.css';
 
-export function PageNotFound() {
+export function PageNotFound({ cart }) {
   return (
     <>
       <title>404 Not Found</title>
       <link rel="icon" href="images/not-found-favicon.jpg" />
-      <Header />
+      <Header cart={cart}/>
       <div class="not-found">
         Page not found :(
       </div>

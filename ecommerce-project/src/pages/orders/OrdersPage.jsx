9fb03e1c33edb86a7c0cc5be52a5eea4db3c7b3a
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-05 08:23:19 +0545

    used async await in orders page

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -11,10 +11,12 @@ export function OrdersPage({cart}) {{
   const [orders, setOrders] = useState(null);
 
   useEffect(() => {
-    axios.get('/api/orders?expand=products')
-      .then((response) => {
-        setOrders(response.data);
-      })
+    const fetchOrdersData = async () => {
+      const response = await axios.get('/api/orders?expand=products');
+      setOrders(response.data);
+    }
+
+    fetchOrdersData();
   }, []);
 
   return (

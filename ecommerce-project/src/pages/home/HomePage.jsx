642e0010a6eb479eabd1d681f7ec44c46e9e7149
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-06 07:45:16 +0545

    Create add to cart feature

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -6,7 +6,7 @@ import { ProductsGrid } from './ProductsGrid';
 // import { products } from '../../starting-code/data/products';
 
 
-export function HomePage({cart}) {
+export function HomePage({cart, loadCart}) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -25,7 +25,7 @@ export function HomePage({cart}) {
     <Header cart={cart} />
 
     <div className="home-page">
-      <ProductsGrid products={products} />
+      <ProductsGrid products={products} loadCart={loadCart} />
     </div>
   </>
   )

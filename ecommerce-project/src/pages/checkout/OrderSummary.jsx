124f0ed8a51da1a63cd6cfa76dc924a4dd811f01
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-06 09:00:57 +0545

    Create delete cart item feature

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
-import { formatCurrency } from '../../utils/money';
+import { CartItemDetails } from './CartItemDetails';
 import { DeliveryOptions } from './DeliveryOptions';
 
 export function OrderSummary({ deliveryOptions, cart, loadCart}) {
@@ -15,30 +15,8 @@ export function OrderSummary({ deliveryOptions, cart, loadCart}) {
             <div className="delivery-date">
               Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
             </div>
-
             <div className="cart-item-details-grid">
-              <img className="product-image"
-                src={cartItem.product.image} />
-
-              <div className="cart-item-details">
-                <div className="product-name">
-                  {cartItem.product.name}
-                </div>
-                <div className="product-price">
-                  {formatCurrency(cartItem.product.priceCents)}
-                </div>
-                <div className="product-quantity">
-                  <span>
-                    Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                  </span>
-                  <span className="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span className="delete-quantity-link link-primary">
-                    Delete
-                  </span>
-                </div>
-              </div>
+              <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
 
               <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart} />
             </div>

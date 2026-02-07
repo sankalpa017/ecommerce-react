b0975c6b56b8dd11ad26fce8ce011657e48bb7b1
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-07 17:03:46 +0545

    Made update quantity feature in checkout page

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,33 @@
 import axios from 'axios';
+import { useState, useEffect, useRef } from 'react';
 import { formatCurrency } from '../../utils/money';
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [updating, setUpdating] = useState(false);
+  const [cartQuantity, setCartQuantity] = useState(cartItem.quantity);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [updating]);
+
+  const updateQuantity = async () => {
+    if (cartQuantity <= 0 || cartQuantity >= 100) {
+      setCartQuantity(cartItem.quantity);
+      setUpdating(false);
+      return;
+    }
+    if (updating) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(cartQuantity)
+      });
+      await loadCart();
+      setUpdating(false)
+    } else {
+      setUpdating(true)
+    }
+  }
+
   const deleteCartItem = async() => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`)
     await loadCart();
@@ -20,10 +46,42 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity: 
+            <input
+              ref={inputRef}
+              type="number"
+              className="update-quantity-input"
+              style={{display: updating ? 'inline-block' : 'none'}}
+              value={cartQuantity}
+              onChange={(event) => {
+                setCartQuantity(event.target.value);
+              }}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  updateQuantity();
+                } else if (event.key === 'Escape') {
+                  setCartQuantity(cartItem.quantity);
+                  setUpdating(false);
+                }
+              }}
+            />
+            <span className="quantity-label"
+              style={{display: updating ? 'none' : 'inline-block'}}
+            >
+              {cartItem.quantity}
+            </span>
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary"
+            onClick={updateQuantity}
+            style={{display: updating ? 'none' : 'inline-block'}}
+          >
             Update
+          </span>
+          <span className="link-primary"
+            onClick={updateQuantity}
+            style={{display: updating ? 'inline-block' : 'none'}}
+          >
+            Save
           </span>
           <span className="delete-quantity-link link-primary"
             onClick={deleteCartItem}

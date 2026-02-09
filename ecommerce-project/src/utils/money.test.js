b577d735cec1ac8bc2928609333ae01436fc9e0c
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-09 14:14:58 +0545

    Setup vitest for integration test

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -17,6 +17,6 @@ describe('formatCurrency', () => {
   });
 
   it('formats 2000.4 cents as $20.00', () => {
-    expect(formatCurrency(2000.4)).toBe('$20.00');
+    expect(formatCurrency(2000.4)).toBe('$20.00'); 
   })
 })

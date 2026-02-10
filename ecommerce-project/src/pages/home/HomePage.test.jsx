0f57561f44ca85dddcbd1fd705948d8bba98ac21
sankalpa017 <sankalpaneupane98989898@gmail.com>
2026-02-10 20:05:34 +0545

    Tested add to cart button on home page

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -1,6 +1,7 @@
 import { it, describe, expect, vi, beforeEach } from 'vitest';
 import { MemoryRouter } from 'react-router';
 import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { HomePage } from './HomePage';
 import axios from 'axios';
 
@@ -8,9 +9,11 @@ vi.mock('axios');
 
 describe('HomePage component', () => {
   let loadCart;
+  let user;
 
   beforeEach(() => {
     loadCart = vi.fn();
+    user = userEvent.setup();
 
     axios.get.mockImplementation(async (urlLink) => {
       if (urlLink === '/api/products') {
@@ -40,7 +43,7 @@ describe('HomePage component', () => {
         }
       }
     });
-  })
+  });
 
   it('displays home page correctly', async () => {
     render(
@@ -60,5 +63,35 @@ describe('HomePage component', () => {
       within(productContainers[1])
         .getByText('Intermediate Size Basketball')
     ).toBeInTheDocument();
-  })
+  });
+
+  it('adds the correct product to the cart', async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+
+    const productContainers = await screen.findAllByTestId('product-container');
+
+    let addToCartButton = within(productContainers[0]).getByTestId('add-to-cart-button');
+
+    await user.click(addToCartButton);
+
+    addToCartButton = within(productContainers[1]).getByTestId('add-to-cart-button');
+
+    await user.click(addToCartButton);
+
+    expect(axios.post).toHaveBeenCalledTimes(2);
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1
+    });
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    });
+
+    expect(loadCart).toHaveBeenCalledTimes(2);
+  });
 })
